0fbca020b837c7fa4461bfc7ec2e3049840e0105
havyoon <bilyknikita18@gmail.com>
2024-06-26 09:40:56 +0200

    initial commit

---
+++ src/main/java/com/example/nikita/dema/DemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.nikita.dema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nikita/dema/DemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nikita.dema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
