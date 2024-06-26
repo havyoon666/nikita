389ed52ec856277c57cfaba29b2bb5e721ba2ce1
dmytro <dmitry.b@scopicsoftware.com>
2024-06-26 11:01:16 +0300

    Something from Dmytro

--- src/main/java/com/example/nikita/dema/DemaApplication.java
+++ src/main/java/com/example/nikita/dema/DemaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemaApplication {
 
     public static void main(String[] args) {
+        System.out.println("ddfdfd");
         SpringApplication.run(DemaApplication.class, args);
     }
 
---
+++ src/main/java/com/example/nikita/dema/services/HisService.java
@@ -0,0 +1,10 @@
+package com.example.nikita.dema.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HisService {
+    public String getString(){
+        return "from his service";
+    }
+}
