dfaecdfd0b23ac1aede29f27a7c87a352b3b6950
dmytro <dmitry.b@scopicsoftware.com>
2024-07-29 10:52:04 +0300

    types

---
+++ src/main/java/com/example/nikita/dema/bases/types/Main.java
@@ -0,0 +1,12 @@
+package com.example.nikita.dema.bases.types;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+//        Student student = new Student("Dmytro", 43);
+//        System.out.println(student.getAge().);
+//        System.out.println(student.getName().);
+        String someValue = "555";
+        Integer i = Integer.valueOf(someValue);
+    }
+}
